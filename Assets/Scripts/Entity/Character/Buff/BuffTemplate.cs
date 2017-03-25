30e35563ea9a1a6ac9d3cd0870a6fda61613c04f
Jim Roberts <jim.d.roberts@gmail.com>
2017-03-25 02:09:43 -0700

    BuffTemplate

--- Assets/Scripts/Entity/Character/Buff/BuffTemplate.cs
+++ Assets/Scripts/Entity/Character/Buff/BuffTemplate.cs
@@ -1,11 +1,42 @@
 ﻿using UnityEngine;
 
-public class BuffTemplate : ScriptableObject
+public class BuffTemplate : DatabaseItem
 {
-	private string name = "NO_NAME";
 	private string description = "NO_DESCRIPTION";
 	private float duration = 0.0f;
 	private int maxStacks = 1;
+
+	[MenuItem("Assets/Create/Templates/Buff/BuffTemplate")]
+	public static ItemTemplate Create()
+	{
+		BuffTemplate template = ScriptableObject.CreateInstance<BuffTemplate>();
+		string path = "Assets/NewBuffTemplate.asset";
+		path = AssetDatabase.GenerateUniqueAssetPath(path);
+		AssetDatabase.CreateAsset(template, path);
+		AssetDatabase.SaveAssets();
+		return template;
+	}
+
+	public override void OnGUILayout()
+	{
+		EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Name:");
+		string newName = GUILayout.TextField(this.name);
+		if (newName != this.name)
+		{
+			string path = AssetDatabase.GetAssetPath(this);
+			AssetDatabase.RenameAsset(path, newName);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Identifier:");
+		GUILayout.TextField(this.Identifier.ToString());
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.EndVertical();
+	}
 
 	void OnApply(GameObject obj)
 	{
---
+++ Assets/Scripts/Entity/Character/Buff/BuffTemplateDatabaseEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class BuffTemplateDatabaseEditor : DatabaseEditor<BuffTemplate>
+{
+	[MenuItem("Templates/Buff Template Database Editor")]
+	static void Init()
+	{
+		EditorWindow.GetWindow<BuffTemplateDatabaseEditor>();
+	}
+}
